d1c127c64ead23d01bf58a9a38706fa20e47b5e4
Miyoshi <321270166@alumno.enp.unam.mx>
2023-06-10 20:22:38 -0600

    <miyoshi><add></>: add the templates for login and register pages

---
+++ dynamics/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--CSS-->
+    <link rel="stylesheet" href="../statics/main.css">
+    <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.css">
+    <!--Meta-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+    <?php include '../templates/login.html'; ?>
+    <!--Scripts-->
+    <script src="../libs/bootstrap/dist/js/bootstrap.js"></script>    
+</body>
+</html>
