aca308aa13ee3e831bf398aab44bc8ccb09940b5
Miyoshi <321270166@alumno.enp.unam.mx>
2023-06-10 20:31:58 -0600

    <miyoshi><add></>: add the final ver to login and register pages

--- dynamics/register.php
+++ dynamics/register.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <!--CSS-->
+    <link rel="stylesheet" href="../statics/main.css">
     <link rel="stylesheet" href="../libs/bootstrap/css/bootstrap.css">
     <!--Meta-->
     <meta charset="UTF-8">
@@ -9,7 +10,7 @@
     <title>Document</title>
 </head>
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
-
+    <?php include '../templates/register.html'; ?>
     <!--Scripts-->
     <script src="../libs/bootstrap/dist/js/bootstrap.js"></script>    
 </body>
